5a4e60ed4391e41e90508f4cccb3af71176fac8d
Loc, Pham <phamducloc2003@gmail.com>
2021-08-19 02:02:07 +0700

    Remove JavaScript dynamic css link generation

--- js/Main.js
+++ js/Main.js
@@ -7,31 +7,98 @@ function navBarToggle() {
   }
 }
 
-window.onload = function(){
-  //Made the html more dynamic
-  let arr = [
-    "C:\\Users\\Pham Loc Duc\\Desktop\\VisualStudioProjects\\Website\\css\\HomePageDesign.css",
-    "C:\\Users\\Pham Loc Duc\\Desktop\\VisualStudioProjects\\Website\\css\\Blogs-And-Articles-Design.css",
-    "C:\\Users\\Pham Loc Duc\\Desktop\\VisualStudioProjects\\Website\\css\\Support-Design.css",
-    "https://fonts.googleapis.com/css?family=Andada+Pro|Roboto|Uchen",
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"];
-    
-    
-    for (var i = 0; i < arr.length; i++){
-      var universalDesign = document.createElement('link');
-      
-      universalDesign.rel = "stylesheet";
-      universalDesign.href = arr[i];
-      
-      document.getElementsByTagName('HEAD')[0].appendChild(universalDesign);
-    }
-    
+var links = {
+  createLinks: function(){
     document.getElementById("home").href = "C:\\Users\\Pham Loc Duc\\Desktop\\VisualStudioProjects\\Website\\html\\HomePage.html";
     document.getElementById("blogs").href = "C:\\Users\\Pham Loc Duc\\Desktop\\VisualStudioProjects\\Website\\html\\Blogs-And-Articles.html";
     document.getElementById("support").href = "C:\\Users\\Pham Loc Duc\\Desktop\\VisualStudioProjects\\Website\\html\\Support.html";
     document.getElementById("about").href = "C:\\Users\\Pham Loc Duc\\Desktop\\VisualStudioProjects\\Website\\html\\About.html";
-  
-    document.getElementById("content-container").innerHTML = "Hi";
   }
+}
+
+var support = {
+  generateSupportContent: function(){
+    var introduction = `
+    <p>Hi :D. If you are here then you're probably thinking about supporting my website, 
+    so I am greatly appreciated for that!
+    <br>
+    <br>
+    The Living Archive is created as a place to inspire and enrich its audience through artistic 
+    and creative visual products of historical topics. Of course, I do not directly run ads
+    on my homepage or my blogs because I want my audience live their experience to the fullest, 
+    which is to be educated and entertained by my artworks and writing. Thus, being distracted by 
+    unrelated ads will absolutely wreck havoc your experience in The Living Archives.
+    <br>
+    <br>
+    However, there are still ways to support me! Check these following options
+    <br>
+    <br>
+    </p>`;
+
+    var fanArtsSupport = `<h2><b>1. Fanarts</b></h2>
+    I'd really appreciated your artworks, letters or literally thing that you 
+    thought it'd be cool to share to me :D.
+    <br>
+    All of your submissions will be posted on the "Fanarts" section of my website.
+    <br>
+    <br>
+    <button class="interactBtn">Submit here!</button>
+    <br>
+    <br>`;
+
+    var donationSupport = `<h2><b>2. Donation</b></h2>
+    <h3><b>a) Paypal</b></h3>
+    You can give me a donation through Paypal (not available).
+    <br>
+    <br>    
+
+    <h3><b>b)Watch an ad</b></h3>
+    You can also watch an ad to support my website!
+    <br>
+    <br>
+    <button class="interactBtn">Watch an ad</button>
+    <br>
+    <br>
+
+    <h3><b>c) Bank transfer (Vietnamese only)</b></h3>
+    If you are Vietnamese and you like my content, you can also give me a donation via my local bank
+    account:
+    <br>
+    1018452173 - SHB - Pham Duc Loc
+    <br>
+    <br>`;
+
+    var feedbackSupport = `
+    <h2><b>2. Feedback</b></h2>
+        Constructive criticism or feedbacks are also a great way for you to support me. This website is created by me but it is meant for you. Your feedback are the most valuable support as it 
+        improve the quality of my content and makes the audience time here much more plausible. 
+        <br>
+        <br>
+        <button class="interactBtn">Fill a feedback form</button>
+        <br>
+        <br>
+    `;
+
+    var finalRegards = `
+    I sincerely thank you! Your support will be a huge motivation for me to keep producing quality content for you to enjoy.
+        <br>
+
+        Hope you have an amazing time reading the content I posted on The Living Archives.
+        <br>
+        <br>    
+        Regards,
+        <br>   
+        <br> 
+        Loc Pham
+    `
+    document.getElementById("content-container").innerHTML = introduction + 
+    fanArtsSupport + feedbackSupport + finalRegards;
+  }
+}
+
+window.onload = function(){
+  links.createLinks();
+  support.generateSupportContent();
+}
 
 
